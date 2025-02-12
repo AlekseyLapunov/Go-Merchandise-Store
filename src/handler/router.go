d3f501a2a15dd7d4afa48a88fa30007265556478
Aleksey Lapunov <lapunoff.alex@yandex.ru>
2025-02-12 21:02:10 +0700

    router placed

--- src/handler/router.go
+++ src/handler/router.go
@@ -2,4 +2,27 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
+func NewRouter() *gin.Engine {
+    router := gin.Default()
+    router.Use(gin.Logger())
 
+    // employeeHandler := TODO
+    // merchHandler    := TODO
+
+    // no jwt
+    authGroup := router.Group("/api")
+    {
+        authGroup.POST("/auth" /*, TODO employeeHandler auth */)
+    }
+
+    // jwt
+    apiGroup := router.Group("/api")
+    apiGroup.Use(/*middleware.Auth()*/)
+    {
+        apiGroup.GET("/info" /*, TODO merchHandler info */)
+        apiGroup.GET("/sendCoin" /*, TODO merchHandler sendCoin*/ )
+        apiGroup.POST("/buy/:item" /*, TODO merchHandler sendCoin*/ )
+    }
+
+    return router
+}
