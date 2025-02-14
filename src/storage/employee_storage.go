1ce0ed5c56dd61aaa02ddda30fcb9bcc7f3f57e3
Aleksey Lapunov <lapunoff.alex@yandex.ru>
2025-02-14 22:09:28 +0700

    fix formatting

--- src/storage/employee_storage.go
+++ src/storage/employee_storage.go
@@ -1,32 +1,32 @@
 package storage
 
 import (
-	"context"
-	"database/sql"
+    "context"
+    "database/sql"
 
-	"github.com/AlekseyLapunov/Go-Merchandise-Store/src/entity"
+    "github.com/AlekseyLapunov/Go-Merchandise-Store/src/entity"
 )
 
 type EmployeeStorage struct {
-	db *sql.DB
+    db *sql.DB
 }
 
 func NewEmployeeStorage(db *sql.DB) EmployeeStorage {
-	return EmployeeStorage{db: db}
+    return EmployeeStorage{db: db}
 }
 
 func (s *EmployeeStorage) GetEmployeeByLogin(ctx context.Context, login string) (*entity.Employee, error) {
-	return nil, nil
+    return nil, nil
 }
 
 func (s *EmployeeStorage) GetBalance(ctx context.Context, employeeID int) (int, error) {
-	return 0, nil
+    return 0, nil
 }
 
 func (s *EmployeeStorage) GetInventory(ctx context.Context, employeeID int) ([]entity.InventoryItem, error) {
-	return []entity.InventoryItem{}, nil
+    return []entity.InventoryItem{}, nil
 }
 
 func (s *EmployeeStorage) GetCoinHistory(ctx context.Context, employeeID int) (*entity.CoinHistory, error) {
-	return nil, nil
+    return nil, nil
 }
