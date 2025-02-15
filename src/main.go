f1ccc0deafe547ed5309d398915c9db7c48d0041
Aleksey Lapunov <lapunoff.alex@yandex.ru>
2025-02-15 11:13:49 +0700

    implement db connection points

--- src/main.go
+++ src/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
     "database/sql"
+    "log"
+    "os"
     "github.com/AlekseyLapunov/Go-Merchandise-Store/src/handler"
     "github.com/AlekseyLapunov/Go-Merchandise-Store/src/usecase"
     "github.com/AlekseyLapunov/Go-Merchandise-Store/src/storage"
+    _ "github.com/lib/pq"
 )
 
 func main() {
-    db, err := sql.Open("", "")
+    const ENV_MERCH_STORE_DB_URL = "MERCH_STORE_DB_URL"
 
+    dsn := os.Getenv(ENV_MERCH_STORE_DB_URL)
+    if dsn == "" {
+        log.Fatalf("\"%s\" environment variable is not set", ENV_MERCH_STORE_DB_URL)
+    }
+
+    db, err := sql.Open("postgres", dsn)
     if err != nil {
-        // TODO graceful shutdown
+        log.Fatalf("Can't connect to the database: %v", err)
     }
     defer db.Close()
+
+    if err := db.Ping(); err != nil {
+        log.Fatalf("Probe ping to the database was not answered: %v", err)
+    }
+    log.Println("Connection to the database is established")
 
     employeeStorage   := storage.NewEmployeeStorage(db)
     merchStorage      := storage.NewMerchStorage(db)
