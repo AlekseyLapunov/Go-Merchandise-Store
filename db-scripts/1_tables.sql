c2d8ae11dfb99f8b8afefdf23d30da8613c7d436
Aleksey Lapunov <lapunoff.alex@yandex.ru>
2025-02-16 07:18:31 +0700

    db upd

--- db-scripts/1_tables.sql
+++ db-scripts/1_tables.sql
@@ -1,7 +1,7 @@
 create table employees (
     id       serial primary key,
     login    varchar(25) unique not null,
-    password varchar(62) not null,
+    password varchar(64) not null,
     coins    int default 0
 );
 
@@ -11,15 +11,15 @@ create table merch (
     cost int check (cost >= 0)
 );
 
-create table purchases (
-    id       serial primary key,
-    emp_id   int references employees(id),
-    merch_id int references merch(id)
-);
-
 create table operations (
     id          serial primary key,
     recv_emp_id int references employees(id),
     send_emp_id int references employees(id),
     amount      int check(amount > 0)
+);
+
+create table purchases (
+    id       serial primary key,
+    emp_id   int references employees(id),
+    merch_id int references merch(id)
 );
